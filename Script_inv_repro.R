7a2c8383dfc9e2bab6d78ba44ff0c9c66e5b2f39
deyvisomar <deyvisomarmq@gmail.com>
2022-10-25 05:59:36 -0400

    Segundo paso de ejecucion de codigo

--- Script_inv_repro.R
+++ Script_inv_repro.R
@@ -1,5 +1,11 @@
 #Comenzando
 library(tidyverse)
-load("base_datos_persona.RData")
+library(repmis)
+source_data("https://github.com/deyvisomar/InvestigacionReproducible/blob/main/base_datos_persona.RData?raw=false")
+#primer cambio efectuado
+base_personas= Persona
+#solo filtro datos de hombres para contar
+datos= base_personas %>% filter(s2_02==1)
 
-
+  
+  
