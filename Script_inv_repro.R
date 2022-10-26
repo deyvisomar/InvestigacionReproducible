4b8cad85d6b7e7fbf4246b443bc24b3d0a4f0cbf
deyvisomar <deyvisomarmq@gmail.com>
2022-10-25 21:57:02 -0400

    primer grafico

--- Script_inv_repro.R
+++ Script_inv_repro.R
@@ -4,7 +4,7 @@ library(plotly)
 library(repmis)
 #source_data("https://github.com/deyvisomar/InvestigacionReproducible/blob/main/base_datos_persona.RData?raw=false")
 #save(parque_automotor, file = "parque_automotor.RData")
-load("parque_automotor.RData")
+#load("parque_automotor.RData")
 #primer cambio efectuado
 #datos= parque_automotor %>% filter(!(PAR_TIPO_VENTA=="ANEXO"))
 datos= parque_automotor %>% filter(DEPARTAMENTO=="LA PAZ")
