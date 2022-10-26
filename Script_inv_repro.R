700a0dbfbe0556d34945fe040e8c2735002ad579
deyvisomar <deyvisomarmq@gmail.com>
2022-10-26 03:22:14 -0400

    primer filtro

--- Script_inv_repro.R
+++ Script_inv_repro.R
@@ -3,11 +3,11 @@ library(tidyverse)
 library(plotly)
 library(repmis)
 #source_data("https://github.com/deyvisomar/InvestigacionReproducible/blob/main/base_datos_persona.RData?raw=false")
-#save(parque_automotor, file = "parque_automotor.RData")
+save(parque_automotor, file = "parque_automotor.RData")
 load("parque_automotor.RData")
 #primer cambio efectuado
 #datos= parque_automotor %>% filter(!(PAR_TIPO_VENTA=="ANEXO"))
-#parque_automotor= parque_automotor[sample(dim(parque_automotor)[1],500000, replace= F),]
+parque_automotor= parque_automotor[sample(dim(parque_automotor)[1],200000, replace= F),]
 #primer gráfico
 datos= parque_automotor %>% filter(DEPARTAMENTO=="LA PAZ")
 p= ggplot(data= datos, aes(x= TIPO_USO, fill=TIPO_VEHICULO))+
